deb488ac0359c5f3c5881100f9df400f9f735ddd
Julian Tochman-Szewc <julian.tochmanszewc@gmail.com>
2019-09-28 18:21:22 +0200

    Eslint added.

--- src/scraper.js
+++ src/scraper.js
@@ -1,68 +1,60 @@
 // @flow
 const rp = require('request-promise');
 const cheerio = require('cheerio');
-const Table = require('cli-table');
 const notifier = require('node-notifier');
 
-
-var oldAd = null;
-var newAd = null;
+let oldAd = null;
+let newAd = null;
 
 const options = {
-    uri: 'https://www.ebay-kleinanzeigen.de/s-berlin/malm-140/k0l3331',
-    transform: function (body) {
-        return cheerio.load(body);
-    }
+  uri: 'https://www.ebay-kleinanzeigen.de/s-berlin/malm-140/k0l3331',
+  transform: function (body) {
+    return cheerio.load(body);
+  }
 };
 
-function push(title, price, link){
-
-    notifier.notify(
-        {
-            title: 'Neue Anzeige!',
-            message: title + "\n" + price,
-            open: link,
-            sound: true, // Only Notification Center or Windows Toasters
-        },
-        function(err, response) {
-            console.log()// Response is response from notification
-        }
-    );
+function push (title, price, link) {
+  notifier.notify(
+    {
+      title: 'Neue Anzeige!',
+      message: title + '\n' + price,
+      open: link,
+      sound: true // Only Notification Center or Windows Toasters
+    },
+    function (err, response) {
+      if (err) console.log(err);// Response is response from notification
+    }
+  );
 }
 
-function getFirstAd() {
-    return rp(options)
-        .then(function ($) {
-            // Process html like you would with jQuery...
-            const title = $("div.aditem-main").find("a.ellipsis")[0].children[0].data;
-            const description = $("div.aditem-main").find("p").not("p.text-module-end")[0].children[0].data;
-            const price = $("div.aditem-details").find("strong")[0].children[0].data;
-            const link = "https://www.ebay-kleinanzeigen.de" + $("div.aditem-main").find("a.ellipsis")[0].attribs.href;
+function getFirstAd () {
+  return rp(options)
+    .then(function ($) {
+      // Process html like you would with jQuery...
+      const mainItem = $('div.aditem-main');
+      const title = mainItem.find('a.ellipsis')[0].children[0].data;
+      const description = mainItem.find('p').not('p.text-module-end')[0].children[0].data;
+      const price = $('div.aditem-details').find('strong')[0].children[0].data;
+      const link = 'https://www.ebay-kleinanzeigen.de' + mainItem.find('a.ellipsis')[0].attribs.href;
 
-            oldAd = newAd;
-            newAd = title;
+      oldAd = newAd;
+      newAd = title;
 
-            if(newAd !== oldAd) {
+      if (newAd !== oldAd) {
+        console.log('\n\n\n\n\n\n\n\n\n');
+        console.log(title);
+        console.log(price);
+        console.log(description);
+        console.log(link);
 
-                console.log("\n\n\n\n\n\n\n\n\n");
-                console.log(title);
-                console.log(price);
-                console.log(description);
-                console.log(link);
-
-                push(title, price, link);
-            }
-
-        })
-
+        push(title, price, link);
+      }
+    });
 }
 
-
-
-setInterval(()=>{
-    getFirstAd()
-        .catch(function (err) {
-            // Crawling failed or Cheerio choked...
-        });
+setInterval(() => {
+  getFirstAd()
+    .catch(function (err) {
+      console.log(err);
+    });
 }, 3000);
-
